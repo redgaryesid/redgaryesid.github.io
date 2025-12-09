23c60d32837b93147d5171d199d79cd1fee0742a
redgaryesid <r.edgaryesid@gmail.com>
2025-12-08 22:14:40 -0500

    Skills en el home

--- src/app/home/page/home.ts
+++ src/app/home/page/home.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
+  skills: any[] = [
+    { name: 'Angular', icon: 'Angular.svg' },
+    { name: 'JavaScript', icon: 'javascript.svg' },
+    { name: 'HTML5', icon: 'html5.svg' },
+    { name: 'CSS3', icon: 'css3.svg' },
+    { name: 'Git', icon: 'git.svg' },
+    { name: 'C#', icon: 'netframework.svg' },
+    { name :'PostgreSQL', icon: 'postgresql.svg' }      
+  ];
 
 }
